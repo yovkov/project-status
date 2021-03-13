86fa58ad7204b101a960167db9bd66100fc385ec
Petar Yovkov <yovkov@gmail.com>
2021-03-13 09:42:58 +0200

    fix port to accept env port

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require ('express');
 
 const app = express();
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
     console.log("listening");
 });
 
